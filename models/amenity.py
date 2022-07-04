59789c9d034d799a278cdf3110ec017ae1af47bd
JosephMamani <jmmollenedo@gmail.com>
2022-07-04 04:51:19 +0000

    Executable files

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Defines Amenity Class
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class saves the attributes of an amenity: name (empty string)
+    """
+    name = ""
